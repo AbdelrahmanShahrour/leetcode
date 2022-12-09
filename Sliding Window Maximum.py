78831c1d2110d51e1af36750a69b150a2db3c4c2
Abdelrahman Shahrour <abdalrahman_shahrour@outlook.com>
2022-12-09 14:32:41 +0300

    day 1

---
+++ Sliding Window Maximum.py
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/sliding-window-maximum/
+
+
+class Solution(object):
+    def maxSlidingWindow(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        output = []
+        q = collections.deque()  # index
+        l = r = 0
+        # O(n) O(n)
+        while r < len(nums):
+            # pop smaller values from q
+            while q and nums[q[-1]] < nums[r]:
+                q.pop()
+            q.append(r)
+
+            # remove left val from window
+            if l > q[0]:
+                q.popleft()
+
+            if (r + 1) >= k:
+                output.append(nums[q[0]])
+                l += 1
+            r += 1
+
+        return output
